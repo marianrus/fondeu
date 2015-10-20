76cdd717cf420034dd47eaf83a9f71d34dab63bd
Marian aws <rusmarianlorin@gmail.com>
2015-10-20 16:05:12 +0000

    homepage production modificaitons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title"> <a href="/dashboard"><b>Panoul de Administrare</b></a>
+		</div>
             </div>
         </div>
     </body>
